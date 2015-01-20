f9a49b560875cab2a8d7d2f5e0ce13a817e8f632
tujoworker <tobias@Tobiass-MacBook-Pro.local>
2015-01-20 19:13:59 +0100

    Init

---
+++ ExpandTextAreaInListView.js
@@ -0,0 +1,58 @@
+var win = Ti.UI.createWindow({backgroundColor: 'white'});
+
+var myTemplate = {
+    properties: {
+        accessoryType: Ti.UI.LIST_ACCESSORY_TYPE_NONE,
+        selectionStyle: Ti.UI.iPhone.ListViewCellSelectionStyle.NONE,
+        width: Ti.UI.FILL,
+        height: Ti.UI.SIZE,
+        height: 100,
+        backgroundColor: 'green',
+        clipMode: Ti.UI.iOS.CLIP_MODE_DISABLED,
+    },
+    childTemplates: [
+        {                            
+            type: 'Ti.UI.TextArea', 
+            bindId: 'input', 
+            properties: {
+                top: 0,
+                bottom: 0,
+                left: 10,
+                right: 10,
+                width: Ti.UI.FILL,
+                height: Ti.UI.SIZE,
+                
+                color: 'black',
+                tintColor: 'black',
+                backgroundColor: '#889f29f9',
+                font: {
+                    fontFamily: 'HelveticaNeue',
+                    fontSize: 16,
+                },
+                scrollable: false,
+                scrollsToTop: false,
+                suppressReturn: false,
+                
+            }
+        },
+    ]
+};
+
+var listView = Ti.UI.createListView({
+    top: 40,
+    templates: { 'template': myTemplate },
+    defaultItemTemplate: 'template',
+});
+var sections = [];
+
+var section = Ti.UI.createListSection();
+var data = [
+    { input: {value: 'Lorem Ipsum er rett og slett dummytekst fra og for trykkeindustrien. Lorem Ipsum har vært bransjens standard for dummytekst END. '}},
+];
+section.setItems(data);
+sections.push(section);
+
+listView.setSections(sections);
+win.add(listView);
+win.open();
+
